d1a8980c5d82a3640d58db03df20430ae27d7dc3
joshua maeba <joshuamaeba@gmail.com>
2023-10-20 16:37:58 +0300

    background change

--- src/components/EmptyChat.jsx
+++ src/components/EmptyChat.jsx
@@ -1,7 +1,7 @@
 export function EmptyChat() {
   return (
     <div className="pb-5 min-h-0 flex-1 flex flex-col items-center justify-center space-y-5">
-      <img src="/logo.jpg" className="h-24" />
+      <img src="/logo.jpeg" className="h-24" />
 
       <h3 className="text-base font-medium text-sky-800">Ask anything...</h3>
     </div>
